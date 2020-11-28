a7769a81c3c3fd6b1f63050582cf46793eb10c40
priyal bhatewara <47683250+priyalbhatewara123@users.noreply.github.com>
2020-11-28 11:40:57 +0530

    splash screen

--- app/src/main/java/com/example/connectin/splashScreen.java
+++ app/src/main/java/com/example/connectin/splashScreen.java
@@ -3,12 +3,52 @@ package com.example.connectin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class splashScreen extends AppCompatActivity {
+
+    //Hooks
+    View first, second, third, fourth, fifth, sixth;
+    TextView a, slogan;
+
+    //Animations
+    Animation topAnimation, bottomAnimation, middleAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+        middleAnimation = AnimationUtils.loadAnimation(this, R.anim.middle_animation);
+
+        //Hooks
+        first = findViewById(R.id.first_line);
+        second = findViewById(R.id.second_line);
+        third = findViewById(R.id.third_line);
+        fourth = findViewById(R.id.fourth_line);
+        fifth = findViewById(R.id.fifth_line);
+        sixth = findViewById(R.id.sixth_line);
+
+        a = findViewById(R.id.a);
+        slogan = findViewById(R.id.tagLine);
+
+        //Setting Animations
+        first.setAnimation(topAnimation);
+        second.setAnimation(topAnimation);
+        third.setAnimation(topAnimation);
+        fourth.setAnimation(topAnimation);
+        fifth.setAnimation(topAnimation);
+        sixth.setAnimation(topAnimation);
+
+        a.setAnimation(middleAnimation);
+        slogan.setAnimation(bottomAnimation);
+
     }
 }
