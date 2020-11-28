2a8e77d6dbf6a832c90320d70c4911de80dee64e
SUKRITI <sukritisinha01>
2020-11-28 13:05:22 +0530

    Fragment Fixed

--- app/src/main/java/com/example/connectin/LoginActivity.java
+++ app/src/main/java/com/example/connectin/LoginActivity.java
@@ -1,14 +1,62 @@
 package com.example.connectin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    float v = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+
+        //Creating Tabs
+        tabLayout.addTab(tabLayout.newTab().setText("LOGIN"));
+        tabLayout.addTab(tabLayout.newTab().setText("SIGN UP"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tabh) {
+                if (tabh.getPosition() == 1) {
+                    viewPager.setCurrentItem(tabh.getPosition());
+                } else {
+                    viewPager.setCurrentItem(tabh.getPosition());
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tabh) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tabh) {
+            }
+        });
+
+        //Adapter
+        final LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(), this, tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        //Animation
+        tabLayout.setTranslationY(300);
+        tabLayout.setAlpha(v);
+        tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(100).start();
+
     }
 }
